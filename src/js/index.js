b05de25a8d54fa83806cfd26abaf1c522a3426cf
Daniel <danchan020@gmail.com>
2022-09-08 15:46:12 -0700

    redeploy

--- src/js/index.js
+++ src/js/index.js
@@ -7,25 +7,25 @@ const route = (event) => {
     handleLocation()
 }
 
-const routes = {
-    404: "/404.html",
-    "/": "/landing.html",
-    "projects": "/projects.html",
-    "blogs": "/blogs.html",
-    "contact" : "/contact.html",
-}
+// const routes = {
+//     404: "/404.html",
+//     "/": "/landing.html",
+//     "projects": "/projects.html",
+//     "blogs": "/blogs.html",
+//     "contact" : "/contact.html",
+// }
 
-const handleLocation = async () => {
-    const path = window.location.pathname
-    const route = routes[path] || routes[404]
-    const html = await fetch(route).then((data) => data.text())
-    document.getElementById("main-nav").innerHTML = html
-}
+// const handleLocation = async () => {
+//     const path = window.location.pathname
+//     const route = routes[path] || routes[404]
+//     const html = await fetch(route).then((data) => data.text())
+//     document.getElementById("main-nav").innerHTML = html
+// }
 
-window.onpopstate = handleLocation
-window.route = route
+// window.onpopstate = handleLocation
+// window.route = route
 
-handleLocation()
+// handleLocation()
 
 
 // message
