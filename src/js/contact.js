78926f8974d3757b11875e0166f3c10c1db1b503
Daniel <danchan020@gmail.com>
2022-09-08 18:00:10 -0700

    contact form focus

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,3 +1,23 @@
+// form input
+
+const inputs = document.querySelectorAll(".form-input");
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", (event) => {
+      const parent = event.target.parentElement;
+      parent.classList.add("focused");
+    });
+  });
+  
+  inputs.forEach((input) => {
+    input.addEventListener("blur", (event) => {
+      const parent = event.target.parentElement;
+      if (!event.target.value) {
+        parent.classList.remove("focused");
+      }
+    });
+  });
+
 // message
 
 const message = document.querySelector("#message")
